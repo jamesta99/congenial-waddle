617c030833efb2289174f8952325495b4387787d
jamesta99 <79133392+jamesta99@users.noreply.github.com>
2021-03-03 23:33:52 -0800

    Fix syntax error in declaration

--- investment.py
+++ investment.py
@@ -1,7 +1,7 @@
-def calculate_apr(int principal, float interest_rate, int years):
+def calculate_apr(principal, interest_rate, years):
 	'''Calculates the APR of an investment given the principal amount, the interest rate, and the time in years'''
 	apr = 0.0
-	if principal < 0 or interest_rate < 0 or years <= 0:
+	if principal < 0 or interest_rate < 0 or years < 1:
 		return False
 	for i in range (years):
 		apr = principal*(1+interest_rate)
