8d1c976d2339629bcc1d2806696348a72039561a
James Ta <jamesta5566@gmail.com>
2021-03-03 21:18:07 -0800

    docs: add docstring to calculate_apr function

--- investment.py
+++ investment.py
@@ -1,9 +1,9 @@
 def calculate_apr(int principal, float interest_rate, int years):
+	'''Calculates the APR of an investment given the principal amount, the interest rate, and the time in years'''
 	apr = 0.0
-	if principal < 0 or interest_rate < 0 or years <= 0:		#checks for valid inputs
+	if principal < 0 or interest_rate < 0 or years <= 0:
 		return False
-	interest_rate += 1
 	for i in range (years):
-		apr = principal * interest_rate
+		apr = principal*(1+interest_rate)
 	return apr
 
