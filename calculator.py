a314a133a15cfe5014428bae5e8f039e091d2b11
James Ta <jamesta5566@gmail.com>
2021-03-03 22:14:31 -0800

    feat: add basic calculator function

---
+++ calculator.py
@@ -0,0 +1,34 @@
+def calculator(float number1, float number2, string operator):
+	'''Recieves two numbers and an operator, then performs an operation using both numbers
+and the operator. Prints the result afterwards'''
+	answer = 0.0
+	if operator == "+":
+		answer = number1 + number2
+	elif operator == "-":
+		answer = number1 - number2
+	elif operator == "*":
+		answer = number1 * number2
+	elif operator == "/":
+		answer = number1 / number2
+	elif operator == "//":
+		answer = number1 // number2
+	elif operator == "**":
+		answer = number1 ** number2
+	else:
+		sys.exit("Invalid operation")
+	print(answer)
+
+def input_output():
+	'''Function for evaluating simple expressions. Asks the user for two numbers and an operator, then passes 
+them to the calculator function for evaluation. After the answer is printed, asks the user to continue or not'''
+	number1 = 0.0
+	number2 = 0.0
+	number1 = int(input("Enter the first number: "))
+	number2 = int(input("Enter the second number: "))
+	operator = input("Enter the operation: ")
+	calculator(number1, number2, operator)
+	terminate = input("Do you wish to exit? ")
+	if terminate == n
+		input_output()
+	elif terminate == y
+		sys.exit("Program ended")
