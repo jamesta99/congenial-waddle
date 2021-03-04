03f7b537ba747bfdcb334ff658999917f1620b05
James Ta <jamesta5566@gmail.com>
2021-03-03 23:09:29 -0800

    docs: add docstring to calculate_time and wrapper functions

--- timing.py
+++ timing.py
@@ -1,6 +1,8 @@
 import time
 def calculate_time(func):
+	'''Decorator that prints the time it takes for a function func to execute'''
 	def wrapper():
+		'''Wrapper that contains the implementation of the decorator'''
 		start_time = time.time()
 		func()
 		print("Total time: ", time.time() - start_time)
